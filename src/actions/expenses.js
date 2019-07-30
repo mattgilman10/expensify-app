afd1d91438a01ee989803a7caeb0edaaf2277cce
Gilman, Matthew (mg1670) <mg1670@att.com>
2019-07-30 12:08:08 -0400

    Setup setExpenses actions class

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -4,7 +4,7 @@ import database from '../firebase/firebase';
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
-   expense
+    expense
 });
 
 export const startAddExpense = (expenseData = {}) => {
@@ -38,3 +38,25 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+//export const startSetExpenses;
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
